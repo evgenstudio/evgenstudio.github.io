b6101fc18d7924af621f21fd2e41f8a5b8261b2e
altair-org <jacking98@bk.ru>
2017-04-09 11:07:33 +0300

    пр

--- borodist/mail.php
+++ borodist/mail.php
@@ -1,12 +1,11 @@
 <?php
 
-$recepient = "youmail@ya.ru";
-$sitename = "Название сайта";
+$recepient = "jacking98@bk.ru";
+$sitename = "Сайт";
 
 $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
-$text = trim($_GET["text"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nТелефон: $phone \n";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
