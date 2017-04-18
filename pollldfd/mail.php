a3c18ca59f6e1ca6cc24b52bbab488d712783f9c
altair-org <jacking98@bk.ru>
2017-04-18 07:55:55 +0300

    м

---
+++ pollldfd/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$recepient = "youmail@ya.ru";
+$sitename = "Полянка";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone \n";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
